93cb7388d9b65fdd3e6e26931e675b047fa1e5e9
Alex Jolly <jolly_alex@yahoo.com>
2013-03-18 19:27:36 -0400

    Initial commit

---
+++ src/com/jollex/Minesweeper/Game.java
@@ -0,0 +1,47 @@
+package com.jollex.Minesweeper;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements MouseListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
