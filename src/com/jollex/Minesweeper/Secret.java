e9b131eeaa006d36fe0a91605c7dc94f60db1b69
Alex Jolly <jolly_alex@yahoo.com>
2013-03-22 08:47:51 -0400

    Added action listeners to buttons and answer labels

--- src/com/jollex/Minesweeper/Secret.java
+++ src/com/jollex/Minesweeper/Secret.java
@@ -1,6 +1,8 @@
 package com.jollex.Minesweeper;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Secret extends JPanel {
@@ -11,6 +13,7 @@ public class Secret extends JPanel {
 	private JPanel promButtons = null;
 	private JButton yes = null;
 	private JButton no = null;
+	private JLabel answer = null;
 	
 	private ImageIcon button = new javax.swing.ImageIcon(getClass().getResource("images/button.gif"));
 	
@@ -21,6 +24,11 @@ public class Secret extends JPanel {
 	}
 	
 	private void setUpProm() {
+		answer = new JLabel();
+		answer.setFont(new Font("Arial", Font.BOLD, 12));
+		this.add(answer);
+		answer.setVisible(false);
+		
 		question = new JLabel();
 		question.setText("<html><center>Janine, will you go" +
 				"<br>to prom with me?" +
@@ -36,15 +44,49 @@ public class Secret extends JPanel {
 		yes.setBorder(BorderFactory.createEmptyBorder());
 		yes.setIconTextGap(-35);
 		yes.setFont(new Font("Arial", Font.BOLD, 12));
+		ActionListener yesClick = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				yes();
+			}
+		};
+		yes.addActionListener(yesClick);
 		
 		no = new JButton("No");
 		no.setIcon(button);
 		no.setBorder(BorderFactory.createEmptyBorder());
 		no.setIconTextGap(-32);
 		no.setFont(new Font("Arial", Font.BOLD, 12));
+		ActionListener noClick = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				no();
+			}
+		};
+		no.addActionListener(noClick);
 		
 		promButtons.add(yes);
 		promButtons.add(no);
 		this.add(promButtons);
+	}
+	
+	private void yes() {
+		question.setVisible(false);
+		yes.setVisible(false);
+		no.setVisible(false);
+		
+		answer.setText("Yay!");
+		answer.setVisible(true);
+	}
+	
+	private void no() {
+		question.setVisible(false);
+		yes.setVisible(false);
+		no.setVisible(false);
+		
+		answer.setText("<html><center>Well..." +
+				"<br>this is awkward" +
+				"</center></html>");
+		answer.setVisible(true);
 	}
 }
